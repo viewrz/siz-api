0667fe9e83eb1e8707f5a664bd8f3c627da1abd7
Julien DAUPHANT <judauphant@gmail.com>
2015-06-10 18:09:54 +0200

    Correct Event test

--- test/EventsSpec.scala
+++ test/EventsSpec.scala
@@ -4,21 +4,30 @@ import org.specs2.runner.JUnitRunner
 import play.api.libs.json.{JsArray, JsString, JsObject, JsValue}
 import play.api.test.Helpers._
 import play.api.test.{FakeRequest, WithApplication}
+import models._
+import java.util.Date
 
 @RunWith(classOf[JUnitRunner])
 class EventsSpec extends Specification {
 
   "Events" should {
     "create a event" in new WithApplication{
+      val storyId = "14339467864855da8fe28614"
+      val newStory = Story(boxes = List(), creationDate = new Date(), id = storyId,
+        slug = "pepper-spray", source = Source("9dLmdVDjg1w","youtube",Some(1592000)), picture = Image("http://img.youtube.com/vi/9dLmdVDjg1w/0.jpg"), title = "Pepper Spray",
+        tags = List("short-films"))
+      Story.collection.insert(newStory)
+
       val jsonBody: JsValue = JsObject(
         Seq("events" ->
           JsObject(Seq(
-            "storyId" -> JsString("128112222233334dddddaaaa"),
-            "type" -> JsString("like"),
-            "tags" -> JsArray(Seq(JsString("dogs"),JsString("animals"))))
+            "storyId" -> JsString(storyId),
+            "type" -> JsString("like")
+            )
           )
         )
       )
+
       val createdToken = contentAsJson(route(FakeRequest(POST, "/tokens").withJsonBody(JsObject(Seq()))).get)
 
       val createdEvent = route(FakeRequest(POST, "/events").withHeaders(("X-Access-Token",(createdToken \ "tokens" \ "id").as[String])).withJsonBody(jsonBody)).get
