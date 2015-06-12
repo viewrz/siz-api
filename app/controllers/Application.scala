63dd31ebf9a01424c14ef2832266b952198ae6d4
Julien DAUPHANT <judauphant@gmail.com>
2015-06-12 16:19:28 +0200

    Implement healt check

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,9 +2,11 @@ package controllers
 
 import actions.{TokenCheckAction, LoggingAction}
 import formats.APIJsonFormats
-import models.TopLevel
+import models._
 import play.api.libs.json.Json
 import play.api.mvc._
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 object Application extends Controller with APIJsonFormats {
 
@@ -18,6 +20,20 @@ object Application extends Controller with APIJsonFormats {
       )
       
       Ok(Json.toJson(TopLevel(links = Some(services))))
+    }
+  }
+
+  def health = LoggingAction {
+    Action.async {
+      request =>
+        Story.findRecommends(12).map {
+          results =>
+            val api = Map("api" -> Map("state" -> "active"))
+            Ok(Json.toJson(api))
+        }.recover {
+          case _ =>
+            InternalServerError(Error.toTopLevelJson(s"Database error: some functionalities could malfunction"))
+        }
     }
   }
 }
