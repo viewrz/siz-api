001f7bcca6d65620c97e7bed4ad6e9024dc3072a
Julien DAUPHANT <judauphant@gmail.com>
2015-06-12 17:44:12 +0200

    Change error code for incorrect password to 404, refactor and add new test case

--- test/TokensSpec.scala
+++ test/TokensSpec.scala
@@ -5,12 +5,16 @@ import play.api.libs.json.JsObject
 import play.api.test.Helpers._
 import play.api.test.{FakeRequest, WithApplication}
 
+object TokenHelper {
+  def createToken() = route(FakeRequest(POST, "/tokens").withJsonBody(JsObject(Seq()))).get
+  def newToken() = (contentAsJson(createToken()) \ "tokens" \ "id").as[String]
+}
+
 @RunWith(classOf[JUnitRunner])
 class TokensSpec extends Specification {
-
   "Tokens" should {
     "create a token" in new WithApplication {
-      val createdToken = route(FakeRequest(POST, "/tokens").withJsonBody(JsObject(Seq()))).get
+      val createdToken = TokenHelper.createToken()
       status(createdToken) must equalTo(CREATED)
 
       contentType(createdToken) must beSome.which(_ == "application/json")
