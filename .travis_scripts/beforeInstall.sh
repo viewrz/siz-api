1f6299c28fdf9c8684fffe7c222ed961c1be4cfe
fred-b <frederic.boulet@gmail.com>
2015-07-20 17:55:28 +0200

    fix mongodb install
    fix mongodb install
    
    fix mongodb install
    
    fix mongodb install
    
    fix mongodb install
    
    fix mongodb install
    
    fix mongodb install mar. juil. 21 10:49:27 CEST 2015
    
    fix mongodb install mar. juil. 21 10:55:45 CEST 2015
    
    fix mongodb install mar. juil. 21 10:58:52 CEST 2015
    
    fix mongodb install mar. juil. 21 11:06:42 CEST 2015

--- .travis_scripts/beforeInstall.sh
+++ .travis_scripts/beforeInstall.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+service mongod stop
+
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
@@ -7,6 +9,5 @@ echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | 
 apt-get update
 apt-get install mongodb-org-server=2.6.10
 apt-get install mongodb-org-shell=2.6.10
-service mongod stop
 
-service mongod start
+#service mongod start
