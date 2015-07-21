0671be00b6857bc7e4bf1c96a696687a445c4996
fred-b <frederic.boulet@gmail.com>
2015-07-21 10:55:45 +0200

    fix mongodb install mar. juil. 21 10:55:45 CEST 2015

--- .travis_scripts/beforeInstall.sh
+++ .travis_scripts/beforeInstall.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+service mongod start
+
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
