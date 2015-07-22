065515a96da954a7bec31e4ea21682815069cc5e
Julien DAUPHANT <judauphant@gmail.com>
2015-07-22 10:37:44 +0200

    Remove v1.0 test

--- test/UsersSpec.scala
+++ test/UsersSpec.scala
@@ -58,24 +58,6 @@ class UsersSpec extends Specification {
 
     }
 
-    "create a email user v1.0" in new WithApplication{
-      val jsonBody: JsValue = JsObject(
-        Seq("users" ->
-          JsObject(Seq(
-            "email" -> JsString("johndoe-v1.0@siz.io"),
-            "password" -> JsString("6b3a55e0261b0304143f805a24924d0c1c44524821305f31d9277843b8a10f4e"),
-            "username" -> JsString("johndoev1.0"))
-          )
-        )
-      )
-      val createUser = route(FakeRequest(POST, "/users").withJsonBody(jsonBody)).get
-
-      status(createUser) must equalTo(CREATED)
-      contentType(createUser) must beSome.which(_ == "application/json")
-      contentAsString(createUser) must contain ("""email""")
-      contentAsString(createUser) must not contain ("""password""")
-    }
-
     "create a email user" in new WithApplication {
       val createdUser = UserHelper.createUser(email,password,username)
 
